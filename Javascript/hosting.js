a29aeb921d4243aeabe1ea2e9ee2773bf1209428
Abhimanyu Kumar <abhimanyukumar072003@gmail.com>
2025-09-05 17:10:36 +0530

    hosting file has been changed

--- Javascript/hosting.js
+++ Javascript/hosting.js
@@ -1,6 +1,6 @@
-//fuction hosting
+//fuction hosting--> while compiling fuction automatically shifted to  the top of the current block of code. 
 
-
+sayMyname("Abhimanyu Kumar")
 function sayMyname(name){
     console.log(name);
 }
